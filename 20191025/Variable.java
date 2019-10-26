e0b98f049fc396d5080780eae372089d653f4f8e
Shanshan-shan <2651351234@qq.com>
2019-10-26 23:36:59 +0800

    1.查看Java数据类型的长度

---
+++ 20191025/Variable.java
@@ -0,0 +1,19 @@
+public class Variable
+{
+    public static void main(String[] args)
+   {
+     long a;
+	 a =13;
+	// short minShort = Short.MIN_VALUE;
+	 //short maxShort = Short.MAX_VALUE;
+	 
+	 //System.out.println(minShort);
+	// System.out.println(maxShort);
+	 
+	 long minLong = long.MIN_VALUE;
+	 long maxLong = long.MAX_VALUE;//
+	 
+	 System.out.println(minLong);
+	 System.out.println(maxLong);
+   }
+}
