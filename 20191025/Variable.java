cd07f0384e41e1b5d2ad9bdc6d2ef65de4ebbcf8
Shanshan-shan <2651351234@qq.com>
2019-10-27 20:05:18 +0800

    提交

--- 20191025/Variable.java
+++ 20191025/Variable.java
@@ -2,18 +2,9 @@ public class Variable
 {
     public static void main(String[] args)
    {
-     long a;
-	 a =13;
-	// short minShort = Short.MIN_VALUE;
-	 //short maxShort = Short.MAX_VALUE;
-	 
-	 //System.out.println(minShort);
-	// System.out.println(maxShort);
-	 
-	 long minLong = long.MIN_VALUE;
-	 long maxLong = long.MAX_VALUE;//
-	 
-	 System.out.println(minLong);
-	 System.out.println(maxLong);
+    
+	System.out.println(args[0]);
+	System.out.println(args[1]);
+	System.out.println(args[2]);
    }
 }
