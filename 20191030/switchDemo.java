3dbf746d945180b0b1996e825417b75b00c55f80
Shanshan-shan <2651351234@qq.com>
2019-11-05 22:33:34 +0800

    switch练习

---
+++ 20191030/switchDemo.java
@@ -0,0 +1,20 @@
+//使用switch语句
+
+public class switchDemo
+{
+    public static void main(String[] args)
+   {
+	   String s = args[0];
+	   switch (s)
+	   {
+		   case "-encoding":
+		   System.out.println("用户指定了编码方式");  //后面要有break；
+		   break;
+		   case "Run":
+		   System.out.println("快跑");
+		   break;
+		   default:
+		   System.out.println("我不知道你在说什么");
+	   }
+   }
+}
