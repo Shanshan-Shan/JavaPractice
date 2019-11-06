e44da88bac9503f40a117be38e05ef5187ae575d
Shanshan-shan <2651351234@qq.com>
2019-11-06 20:42:06 +0800

    数组练习2--冒泡排序

--- 20191106/arrayPractice1.java
+++ 20191106/arrayPractice1.java
@@ -1,6 +1,7 @@
+//数组练习
 
-//数组练习--二分查找；
-
+//1.二分查找；
+/*
 public class arrayPractice1{
 	
 	public static int binarySearch(int[] a,int value){
@@ -23,6 +24,35 @@ public class arrayPractice1{
 		System.out.println(binarySearch(arr,4));
 	}
 	
+}
+*/
+
+//2.冒泡排序，默认数组从小到大排序
+
+import java.util.Arrays;
+
+public class arrayPractice1{
+	 public static void swap(int[] a,int i,int j){
+		 int tmp=a[i];
+		 a[i]=a[j];
+		 a[j]=tmp;
+	}
+	
+	public static void bubbleSort(int[] arr){
+		for(int i = 0; i < arr.length - 1; i++){
+			for (int j = 0; j< arr.length - i -1;j++){
+				if(arr[j]>arr[j+1]){
+					swap(arr,j,j+1);
+				}
+			}
+		}	
+	}		
+    public static void main(String[] args)
+   {
+	   int[] arr = {1,3,2,5,4};
+	   bubbleSort(arr);
+	   System.out.println(Arrays.toString(arr));
+   }
 }
 
 
