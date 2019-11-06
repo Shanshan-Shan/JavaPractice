4bf361e91721621814f178ab8327a2168edc23c9
Shanshan-shan <2651351234@qq.com>
2019-11-06 20:25:48 +0800

    数组练习--二分查找

---
+++ 20191106/arrayPractice1.java
@@ -0,0 +1,31 @@
+
+//数组练习--二分查找；
+
+public class arrayPractice1{
+	
+	public static int binarySearch(int[] a,int value){
+		int left=0;
+		int right=a.length-1;
+		while(left<=right){
+			int mid=(right+left)/2;
+			if(value<a[mid]){        //去左边区间找
+				right=mid-1;
+			}else if(value>a[mid]){  //去右边区间找
+				left=mid +1;
+			}else
+				return mid;
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args){
+		int[] arr = {1,2,3,4,5};
+		System.out.println(binarySearch(arr,4));
+	}
+	
+}
+
+
+
+
+
