fdb9e794350e977aaf7601a7f0c260d28b16de9a
Shanshan-shan <2651351234@qq.com>
2019-11-07 17:26:12 +0800

    更新代码

---
+++ arrayTest/arrayPractice3.java
@@ -0,0 +1,23 @@
+//数组练习--数组常用方法，查文档
+
+
+public class arrayPractice3{
+	
+	public static int copyOfRange(int[] a,int from,int to){
+		int[] dest =new int 
+	}
+	
+	
+	public static int fill(int[] a,int Val){
+		for(int i =from;i<to; i++){
+			ai[i] =Val;
+		}
+	}
+	
+	public static void main(String[] args){
+		int[] arr = {1,2,3,4,5};
+		copyOfRange();
+		System.out.println(binarySearch(arr,4));
+	}
+	
+}
