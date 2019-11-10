5eef3a964d2f8a416a9b4d1be85e83dc444c8241
Shanshan-shan <2651351234@qq.com>
2019-11-10 09:50:00 +0800

    1.定义类 2.使用类

---
+++ classTest/Main.java
@@ -0,0 +1,27 @@
+//创建一个类
+class Person{
+	//定义类的属性
+	String name;
+	int age;
+	
+	//定义类的方法（普通方法）
+	public void sayHello(String arr){
+		System.out.println(name + " say hello to " + arr);
+	}
+	
+}
+
+
+
+public class Main{
+	public static void main(String[] args){
+		Person p = new Person();
+		
+		p.name = "HSS";
+		p.age = 18;
+		
+		p.sayHello("LYS");
+		
+	}
+	
+}
