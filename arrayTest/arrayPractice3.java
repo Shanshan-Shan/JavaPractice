db19b5f0a4b310114cb8ea37c766b9eb09c6d265
Shanshan-shan <2651351234@qq.com>
2019-11-12 11:15:10 +0800

    深入理解数组

--- arrayTest/arrayPractice3.java
+++ arrayTest/arrayPractice3.java
@@ -1,23 +1,19 @@
-//数组练习--数组常用方法，查文档
+//数组练习
 
+import java.util.Arrays;
 
-public class arrayPractice3{
-	
-	public static int copyOfRange(int[] a,int from,int to){
-		int[] dest =new int 
+public class arrayPractice3 {
+	public static void swap(int[] a, int[] b) {
+		int[] t = a;
+		a = b;
+		b = t;
 	}
 	
-	
-	public static int fill(int[] a,int Val){
-		for(int i =from;i<to; i++){
-			ai[i] =Val;
-		}
+	public static void main(String[] args) {
+		int[] ar1 = { 1, 2, 3, 4 };
+		int[] ar2 = { 9, 7, 3, 2 };
+		swap(ar1, ar2);
+		System.out.println(Arrays.toString(ar1));
+		System.out.println(Arrays.toString(ar2));
 	}
-	
-	public static void main(String[] args){
-		int[] arr = {1,2,3,4,5};
-		copyOfRange();
-		System.out.println(binarySearch(arr,4));
-	}
-	
 }
