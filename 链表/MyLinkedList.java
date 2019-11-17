0703b2523c65d832311c18e5c20978f994e30357
Shanshan-shan <2651351234@qq.com>
2019-11-17 14:58:55 +0800

    练习链表的头插头删

---
+++ 链表/MyLinkedList.java
@@ -0,0 +1,62 @@
+//l练习链表的头插和头删
+
+public class MyLinkedList {
+	public static Node buildLinkedListManual() {
+		Node n1 = new Node(1);
+		Node n2 = new Node(2);
+		Node n3 = new Node(3);
+		Node n4 = new Node(4);
+		Node n5 = new Node(5);
+		
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
+	
+		return n1;
+	}
+
+	//链表的打印
+	public static void printLinkedList(Node head) {
+		Node cur = head ;
+		
+		while(cur != null) {
+			System.out.println(cur.val);
+			cur = cur.next;
+		}
+	}
+	
+	//实现链表的头插
+	public static Node pushFront(Node head, int val) {
+		Node node = new Node(val);
+		node.next = head;
+		return node;	
+	}
+
+	//实现链表的头删
+	public static Node popFront(Node head) {
+		if (head == null) {                                    //==
+			throw new RuntimeException("空链表");
+		}
+		return head.next;
+		
+	}
+	
+	public static void main(String[] args) {
+		//
+		Node head = null;
+     	head = pushFront(head,0);  //实现链表的头插
+		head = pushFront(head,1); 
+		head = pushFront(head,2); 
+		head = pushFront(head,3); 
+		head = pushFront(head,4); 
+		
+		printLinkedList(head);
+		
+		head = popFront(head); //头删
+		head = popFront(head);
+		
+		printLinkedList(head);
+		
+	}
+}
---
+++ 链表/Node.java
@@ -0,0 +1,22 @@
+//自己建节点
+
+public class Node {
+	
+	int val;
+	Node next;
+	
+	Node(int val, Node next) {
+		this.val = val;
+		this.next = next;
+	}
+	
+	Node(int val) {
+		this(val, null);
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Node{%d}", val);
+	}
+
+}
