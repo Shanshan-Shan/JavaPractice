a4e21c35508a0ac1ad95f83f74434284272c7fd1
Shanshan-shan <2651351234@qq.com>
2019-12-08 15:54:10 +0800

    单链表实现队列

---
+++ 20191208-队列/src/queue.java
@@ -0,0 +1,86 @@
+//单链表实现队列--先进先出
+//尾插头删的操作
+//头删--o(1)--入队offer
+//出队--头删  poll
+class Node{
+    int val;
+    Node next;
+
+    Node(int val, Node next){
+        this.val = val;
+        this.next = next;
+    }
+    //只有一个节点的情况
+    Node(int val){
+        this(val, null);
+    }
+}
+
+public class queue {
+    private Node front = null;
+    private Node rear = null;
+    private int size = 0;
+
+    public void offer(int val){
+        Node newNode = new Node(val);
+
+        //空队列
+        if(front == null){
+            front = rear = newNode;
+        } else {  //尾插
+            rear.next = newNode;
+            rear = rear.next;
+        }
+        size++;
+    }
+
+    public void poll(){
+        if(front == null){
+            return;
+        }
+        front = front.next;
+        size--;
+    }
+
+    public int peek(){
+        if(front == null){
+            return  -1;
+        }
+        return front.val;
+    }
+
+    //获取队尾的元素值
+    public int rear(){
+        if(front == null){
+            return -1 ;
+        }
+        return rear.val;
+        //rear = null;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean empty(){
+        return size == 0;
+    }
+
+    public static void main(String[] args) {
+        queue q = new queue();
+        System.out.println(q.empty());
+        System.out.println(q.size);
+        q.offer(1);
+        q.offer(3);
+                q.offer(5);
+                q.offer(7);
+                System.out.println(q.empty());
+                System.out.println(q.size);
+                System.out.println(q.peek());
+                q.poll();
+                q.poll();
+                System.out.println(q.empty());
+                System.out.println(q.size);
+                System.out.println(q.peek());
+    }
+}
