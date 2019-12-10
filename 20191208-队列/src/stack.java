7eaa098b0e80366a8653ccc622f8e05e44849930
Shanshan-shan <2651351234@qq.com>
2019-12-10 14:54:40 +0800

    顺序表实现栈

--- 20191208-队列/src/stack.java
+++ 20191208-队列/src/stack.java
@@ -1,24 +1,29 @@
 import java.util.Arrays;
-import java.util.Stack;
 
 //顺序表实现栈
-public class stack {
-    private int[] array = new int[100];
-    private int size = 0;
+public class Stack {
 
-    public void push(int val){
+    private int[] array;
+    private int size;
+
+    public Stack(int n) {
+        array = new int[n];
+        size = 0;
+    }
+
+    public void push(int val) {
         //尾插
         //检查容量
-        if(size == array.length){
-            //增容：申请空间，拷贝
+        if (size == array.length) {
             array = Arrays.copyOf(array, 2 * size);
         }
+        //尾插
         array[size++] = val;
     }
 
     public void pop(){
         if(size > 0)
-            size--;
+            --size;
     }
 
     public int peek(){
@@ -28,22 +33,30 @@ public class stack {
     public int size(){
         return size;
     }
-
     public boolean empty(){
         return size == 0;
     }
 
     public static void main(String[] args) {
-        Stack st = new Stack();
+        Stack st = new Stack(2);
+        System.out.println(st.size());
+        System.out.println(st.empty());
         st.push(1);
         st.push(2);
         st.push(3);
         st.push(4);
-        System.out.println(st.peek());
         st.push(5);
         System.out.println(st.size());
         System.out.println(st.empty());
-        st.pop();
-        System.out.println(st.peek());
+        System.out.println("top: " + st.peek());
+
+        System.out.println("遍历:");
+        while(!st.empty()){
+            System.out.print(st.peek() + " ");
+            st.pop();
+        }
+        System.out.println();
+        System.out.println(st.size());
+        System.out.println(st.empty());
     }
 }
