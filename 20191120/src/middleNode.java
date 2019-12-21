d7118e9dfa7e9a1b3df68b00d6f4e688e0c91f81
Shanshan-shan <2651351234@qq.com>
2019-12-21 22:34:53 +0800

    返回链表的中间节点

---
+++ 20191120/src/middleNode.java
@@ -0,0 +1,23 @@
+//返回链表的中间节点
+
+public class middleNode {
+    public static Node middleNode() {
+    //先求链表长度
+        int count = 0;
+        Node cur = head;
+        while() {
+
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Node n5 = new Node(4);  //
+        Node n4 = new Node(7, n5);
+        Node n3 = new Node(1, n4);
+        Node n2 = new Node(2, n3);
+        Node n1 = new Node(5, n2);
+
+
+    }
+}
