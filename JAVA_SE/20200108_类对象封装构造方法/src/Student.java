982d6028dc57db095bd8bdf42bbd96e26566d13a
Shanshan-shan <2651351234@qq.com>
2020-01-08 17:03:09 +0800

    标准类

---
+++ JAVA_SE/20200108_类对象封装构造方法/src/Student.java
@@ -0,0 +1,30 @@
+//类对象，封装，构造方法综合练习
+
+public class Student {
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
