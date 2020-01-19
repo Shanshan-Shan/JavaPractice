5578d5cfbbfd25f80708bff431ff3675072a3554
Shanshan-shan <2651351234@qq.com>
2020-01-19 22:49:44 +0800

    继承

---
+++ JAVA_SE/继承、super、抽象类/src/Animal.java
@@ -0,0 +1,8 @@
+public abstract class Animal {
+        // 这是一个抽象方法，代表吃东西，但是具体吃什么（大括号的内容）不确定。
+        public abstract void eat();
+        // 这是普通的成员方法
+//    public void normalMethod() {
+//
+//    }
+}
