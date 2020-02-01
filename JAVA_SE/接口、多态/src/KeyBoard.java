1cd83c2c114e07ac4b63e0b7ca932075fc9b0296
Shanshan-shan <2651351234@qq.com>
2020-02-01 19:51:44 +0800

    键盘类实现接口

---
+++ JAVA_SE/接口、多态/src/KeyBoard.java
@@ -0,0 +1,12 @@
+//键盘类，打开/关闭键盘
+public class KeyBoard implements USB{
+    @Override
+    public void turnOn() {
+        System.out.println("打开键盘");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("关闭键盘");
+    }
+}
