e7cd101d1558d97821f258542b4708bd975839b6
Shanshan-shan <2651351234@qq.com>
2020-02-11 19:29:33 +0800

    最后一个单词的长度

--- Test/Test1/src/Test4/Code58.java
+++ Test/Test1/src/Test4/Code58.java
@@ -3,6 +3,8 @@ package Test4;
 //给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。
 //如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。如果不存在最后一个单词，请返回0。
 
+//思路：倒序遍历
+//charAt-获取字符串指定位置单个字符
 public class Code58 {
     public int lengthOfLastWord(String s) {
         int count = 0;
