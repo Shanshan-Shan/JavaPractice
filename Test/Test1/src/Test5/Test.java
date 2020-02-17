8632bd01e75406504d1b4d5aa05198caa96a76ba
Shanshan-shan <2651351234@qq.com>
2020-02-17 08:59:50 +0800

    test

---
+++ Test/Test1/src/Test5/Test.java
@@ -0,0 +1,10 @@
+package Test5;
+
+public class Test {
+        private float f = 1.0f;
+        int m = 12;
+        static int n = 1;
+        public static void main (String args[]) {
+            Test t = new Test();
+        }
+}
