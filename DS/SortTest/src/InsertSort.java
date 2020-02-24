fc55b974c74095c5d6721b088ae7f7c515f18b10
Shanshan-shan <2651351234@qq.com>
2020-02-24 22:07:02 +0800

    插入排序

---
+++ DS/SortTest/src/InsertSort.java
@@ -0,0 +1,30 @@
+//简单排序--直接插入排序
+
+//找插入位置
+public class InsertSort {
+    public static void sort(long[] arr){
+        long tmp = 0;
+        for (int i = 1; i < arr.length; i++) {
+            tmp = arr[i];
+            int j = i;
+            while (j > 0 && arr[j] >= tmp){
+                arr[j] = arr[j-1]; //右移
+                j--;
+            }
+            arr[j] = tmp; //插入位置
+        }
+    }
+    public static void display(long[] arr){
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("]");
+    }
+    public static void main(String[] args) {
+        long[] arr = {34,23,12,1,-1,5}; //-1,1,5,12,23,,34
+        display(arr);
+        sort(arr);
+        display(arr);
+    }
+}
