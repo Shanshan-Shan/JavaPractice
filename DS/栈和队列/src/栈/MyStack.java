8ad2dd248a694fe0702b4400f065ea2b5d8aaf5c
Shanshan-shan <2651351234@qq.com>
2020-02-27 12:16:52 +0800

    实现栈

--- DS/栈和队列/src/栈/MyStack.java
+++ DS/栈和队列/src/栈/MyStack.java
@@ -2,8 +2,20 @@ package 栈;
 
 public class MyStack {
     //底层实现是一个数组
-    private long[] arr = new long[4];
-    private int top = -1; //-1表示栈顶元素为空，--栈为空，因为数组第一个下标是0
+    private long[] arr;
+    private int top; //-1表示栈顶元素为空，--栈为空，因为数组第一个下标是0
+
+    //无参数构造方法
+    public MyStack() {
+        long[] arr = new long[10];
+        top = -1;
+    }
+    //有参数构造方法
+    public MyStack(int maxsize){
+        arr = new long[maxsize];
+        top = -1;
+
+    }
 
     //插入数据
     public void push(int val){
@@ -30,13 +42,5 @@ public class MyStack {
         return top == arr.length-1;
     }
 
-    public static void main(String[] args) {
-        MyStack myStack = new MyStack();
-        myStack.push(3);
-        myStack.push(5);
-        myStack.push(7);
-        myStack.push(9);
-        System.out.println(myStack.isEmpty());
-        System.out.println(myStack.isFull());
-    }
+
 }
