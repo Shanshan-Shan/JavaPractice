46e101b9a61b380f63a8d0b739d99f21c4266777
Shanshan-shan <2651351234@qq.com>
2020-02-29 09:38:08 +0800

    二叉树

---
+++ DS/Tree/src/二叉树/Tree.java
@@ -0,0 +1,24 @@
+package 二叉树;
+
+//二叉树类
+public class Tree {
+    //根节点
+    private Node root;
+
+    //插入节点
+    public void insert(int val){
+
+    }
+
+    //查找节点
+    public void find(int val){
+
+    }
+
+    //删除节点
+    public void remove(int val){
+
+    }
+
+
+}
