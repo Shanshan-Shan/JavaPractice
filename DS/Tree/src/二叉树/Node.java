252dd774b9258cd57e77177d8687461a7e29845e
Shanshan-shan <2651351234@qq.com>
2020-02-29 09:37:52 +0800

    二叉树的表示--节点

---
+++ DS/Tree/src/二叉树/Node.java
@@ -0,0 +1,40 @@
+package 二叉树;
+//二叉树的表现形式--节点
+public class Node {
+    private int val;
+    private Node left;
+    private Node right;
+
+    public Node() {
+    }
+
+    public Node(int val, Node left, Node right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+}
