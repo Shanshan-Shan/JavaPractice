f08d7fdc2b504e91389a0aeb39852e8e96705565
Shanshan-shan <2651351234@qq.com>
2020-03-05 21:12:12 +0800

    二叉树基本操作

--- DS/Tree/src/二叉树/Tree.java
+++ DS/Tree/src/二叉树/Tree.java
@@ -3,34 +3,45 @@ package 二叉树;
 //二叉树类
 public class Tree {
     //根节点
-    private Node root;
+    public Node root;
 
     //插入节点
-    public void insert(int val){
+    public void insert(int val) {
         Node newNode = new Node(val);//封装节点
         Node cur = root;//引用当前节点
         Node parent;    //引用父节点
 
-        if(root == null){        //插入第一个节点，root 为空
+        if (root == null) {        //插入第一个节点，root 为空
             root = newNode;
             return;
-        }else {
+        } else {
+            while (true) {
+                parent = cur;
+                if (cur.data > val) {
+                    cur = cur.left;
+                    if(cur == null){
+                        parent.left = newNode;
+                        return;
+                    }
+                } else {
+                    cur = cur.right;
+                    if(cur == null){
+                        parent.right = newNode;
+                        return;
+
+                    }
+                }
+            }
+        }
+    }
+
+        //查找节点
+        public void find ( int val){
 
         }
 
+        //删除节点
+        public void remove ( int val){
 
-
-    }
-
-    //查找节点
-    public void find(int val){
-
-    }
-
-    //删除节点
-    public void remove(int val){
-
-    }
-
-
+        }
 }
