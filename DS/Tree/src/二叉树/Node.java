e4d3cf26f682b09b91337d81e3a8177c297fdc89
Shanshan-shan <2651351234@qq.com>
2020-03-05 21:11:59 +0800

    二叉树基本操作

--- DS/Tree/src/二叉树/Node.java
+++ DS/Tree/src/二叉树/Node.java
@@ -1,40 +1,13 @@
 package 二叉树;
 //二叉树的表现形式--节点
 public class Node {
-    public int val;
+    public int data;
     public Node left;
     public Node right;
 
-    public Node() {
+    //构造方法
+    public Node(int data) {
+        this.data = data;
     }
 
-    public Node(int val, Node left, Node right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-
-    public int getVal() {
-        return val;
-    }
-
-    public Node getLeft() {
-        return left;
-    }
-
-    public Node getRight() {
-        return right;
-    }
-
-    public void setVal(int val) {
-        this.val = val;
-    }
-
-    public void setLeft(Node left) {
-        this.left = left;
-    }
-
-    public void setRight(Node right) {
-        this.right = right;
-    }
 }
